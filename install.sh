95e92f3beb9cf692e55932408c4136ed7c18d857
Fernando Videira <tragedy_twisty.0t@icloud.com>
2024-03-17 05:32:30 +0000

    Last chance tonight ;-;

--- install.sh
+++ install.sh
@@ -102,10 +102,7 @@ if ask "Change shell to zsh?"; then
         git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
         ~/.fzf/install
 
-        # Set plugins
-        plugins="plugins=(git zsh-autosuggestions zsh-syntax-highlighting web-search gh)"
-
-        sed -i 's/plugins=(git)/$plugins/' ~/.zshrc 
+        sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting web-search gh)/' ~/.zshrc 
         #Add zsh-interactive-cd plugin
         echo "source $ZSH/plugins/zsh-interactive-cd/zsh-interactive-cd.plugin.zsh" >> ~/.zshrc
         echo "source $ZSH/oh-my-zsh.sh" >> ~/.zshrc
