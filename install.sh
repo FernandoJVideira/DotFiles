309b20cf824cf76655f5b2cc5411cc3e8c885277
marcofpadeiro <2231953@my.ipleiria.pt>
2024-03-17 11:31:36 +0000

    This was 100% tested and is working 100%, did all unit tests and stuff /s

--- install.sh
+++ install.sh
@@ -1,50 +1,54 @@
 #!/bin/bash
 
 os=$(uname)
+package_manager=""
+
+if [ "$os" == "Darwin" ]; then
+    package_manager="brew"
+elif [ "$os" == "Linux" ]; then
+    package_manager=$(type -p apt > /dev/null && echo "apt"; type -p pacman > /dev/null && echo "pacman"; type -p dnf > /dev/null && echo "dnf")
+fi
+
+echo "Detected OS: $os"
+echo "Detected Package Manager: $package_manager"
 
 # Ask Y/n
 function ask() {
     read -p "$1 (Y/n): " resp
-    if [ -z "$resp" ]; then
-        response_lc="y" # empty is Yes
-    else
-        response_lc=$(echo "$resp" | tr '[:upper:]' '[:lower:]') # case insensitive
-    fi
-
-    [ "$response_lc" = "y" ]
+    [[ "${resp,,}" =~ ^(y|)$ ]]
 }
 
-function install() {
-    if [ "$os" == "Darwin" ]; then
-        brew install $1
-    else
-        if type apt >/dev/null 2>&1 ; then
-            sudo apt install $1
-        elif type pacman >/dev/null 2>&1 ; then
-            sudo pacman -S $1
-        elif type dnf >/dev/null 2>&1 ; then
-            sudo dnf install $1
-        fi
-    fi
+
+function install_package() {
+    case $package_manager in
+        "brew")
+            brew install "$@"
+            ;;
+        "apt")
+            sudo apt install "$@"
+            ;;
+        "pacman")
+            sudo pacman -S "$@"
+            ;;
+        "dnf")
+            sudo dnf install "$@"
+            ;;
+        *)
+            echo "Unknown package manager"
+            exit 1
+            ;;
+    esac
 }
 
 echo "Current shell: $SHELL"
 shell=$(basename $SHELL)
 
-if [ "$shell" == "zsh" ]; then
+if [ -f /usr/bin/zsh ]; then
     echo "Zsh is already installed"
 else
     echo "Zsh is not installed"
     echo "Installing zsh..."
-    if [ "$os" == "Linux" ]; then
-        if type apt >/dev/null 2>&1 ; then
-            sudo apt install zsh
-        elif type pacman >/dev/null 2>&1 ; then
-            sudo pacman -S zsh
-        elif type dnf >/dev/null 2>&1 ; then
-            sudo dnf install zsh
-        fi
-    fi
+    install_package zsh
 fi
 
 if ask "Change shell to zsh?"; then
@@ -52,7 +56,7 @@ if ask "Change shell to zsh?"; then
         echo "Installing oh-my-zsh..."
         if ! command -v curl >/dev/null 2>&1 ; then
             # Install curl
-            install curl
+            install_package curl
         fi
         # Install oh-my-zsh
         sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -68,26 +72,13 @@ if ask "Change shell to zsh?"; then
         fi
     done
 
+    declare -A distros_aliases
+    distros_aliases=(["brew"]="shell/mac/macos_aliases.sh" ["pacman"]="shell/arch/arch_aliases.sh" ["dnf"]="shell/fedora/fedora_aliases.sh" ["apt"]="shell/debian/debian_aliases.sh")
+
     # Source Distro-specific aliases
-    if [ "$os" == "Darwin" ]; then
-        echo "Installing MacOS Aliases..."
-        fullpath=$(realpath shell/mac/macos_aliases.sh)
-        echo "source $fullpath" >> ~/.zshrc
-    else
-        if type apt >/dev/null 2>&1 ; then
-            echo "Installing Ubuntu/Debian Settings..."
-            fullpath=$(realpath shell/debian/debian_aliases.sh)
-            echo "source $fullpath" >> ~/.zshrc
-        elif type pacman >/dev/null 2>&1 ; then
-            echo "Installing Arch-based Aliases..."
-            fullpath=$(realpath shell/arch/arch_aliases.sh)
-            echo "source $fullpath" >> ~/.zshrc
-        elif type dnf >/dev/null 2>&1 ; then
-            echo "Installing Fedora Aliases..."
-            fullpath=$(realpath shell/fedora/fedora_aliases.sh)
-            echo "source $fullpath" >> ~/.zshrc
-        fi
-    fi
+    echo "Installing $package_manager-based Aliases..."
+    fullpath=$(realpath ${distros_aliases[$package_manager]})
+    echo "source $fullpath" >> ~/.zshrc
 
     if ask "Install and set zsh plugins?"; then
         #Load ZSH plugins
@@ -116,44 +107,35 @@ fi
 
 if ask "Install conda?"; then
     echo "Installing conda..."
-    if [ "$os" == "Darwin" ]; then
-        mkdir -p ~/miniconda3
-        curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -o ~/miniconda3/miniconda.sh
-        bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-        rm -rf ~/miniconda3/miniconda.sh
-    else
-        mkdir -p ~/miniconda3
-        wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-        bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-        rm -rf ~/miniconda3/miniconda.sh
-    fi
+
+    declare -A os_name
+    os_name=(["Darwin"]="MacOSX-arm64" ["Linux"]="Linux-x86_64")
+
+    mkdir -p ~/miniconda3
+    curl https://repo.anaconda.com/miniconda/Miniconda3-latest-${os_name[$os]}.sh -o ~/miniconda3/miniconda.sh
+    bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+    rm -rf ~/miniconda3/miniconda.sh
+
     # Add conda to path
-    if [ "$shell" == "zsh" ]; then
-        echo "export PATH=~/miniconda3/bin:$PATH" >> ~/.zshrc
-    elif [ "$shell" == "bash" ]; then
-        echo "export PATH=~/miniconda3/bin:$PATH" >> ~/.bashrc
-    fi
+    echo "export PATH=~/miniconda3/bin:$PATH" >> ~/.${shell}rc
 fi
 
 if ask "Install exa?"; then
     echo "Installing exa..."
-    install exa
+    install_package exa
 fi
 
 if ask "Install Homebrew?"; then
     echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     # Add Homebrew to path
-    if [ "$shell" == "zsh" ]; then
-        echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.zshrc
-    elif [ "$shell" == "bash" ]; then
-        echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.bashrc
-    fi
+
+    echo "export PATH=~/miniconda3/bin:$PATH" >> ~/.${shell}rc
 fi
 
 if ask "Install tmux?"; then
     echo "Installing tmux..."
-    install tmux
+    install_package tmux
 fi
 
 # Tmux conf
