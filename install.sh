69d86d2c37e55f9cf5a75a0740e5eb5528bb766c
Fernando Videira <tragedy_twisty.0t@icloud.com>
2024-03-17 04:14:57 +0000

    Change in Ask function

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Ask Y/n
 function ask() {
-    read -p "$1 [Y/n] " resp
-    [-z "$resp"] || [ "$resp" == "Y" ] || [ "$resp" == "y" ]
+    read -p "$1 (Y/n): " resp
+    if [ -z "$resp" ]; then
+        response_lc="y" # empty is Yes
+    else
+        response_lc=$(echo "$resp" | tr '[:upper:]' '[:lower:]') # case insensitive
+    fi
+
+    [ "$response_lc" = "y" ]
 }
 
 os=$(uname)
