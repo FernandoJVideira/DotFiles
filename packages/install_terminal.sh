0deb74c7c2325f912a53e6f6c292730a54a2d212
Fernando Videira <tragedy_twisty.0t@icloud.com>
2024-03-22 01:04:42 +0000

    Idk anymore

--- packages/install_terminal.sh
+++ packages/install_terminal.sh
@@ -5,9 +5,10 @@ source packages/install_packages.sh
 
 if [ "$package_manager" = "brew" ]; then
     install_package iterm2
+    install_package warp
     cp config/com.googlecode.iterm2.plist ~/Library/Preferences
 else
     install_package kitty
-    fullpath=$(realpath config/kitty.conf)
+    fullpath=$(realpath package/config/kitty.conf)
     cp $fullpath ~/.config/kitty/kitty.conf
 fi
