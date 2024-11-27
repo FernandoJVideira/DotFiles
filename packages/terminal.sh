531b328b148c70cae1f7c71cd6930c23fb79a059
FernandoJVideira <03.pleaser-minster@icloud.com>
2024-11-27 18:47:23 +0000

    fix:small fixes

--- packages/terminal.sh
+++ packages/terminal.sh
@@ -9,8 +9,24 @@ if [ "$os" = "Darwin" ]; then
     brew install --cask wezterm
     brew install starship
 else
-    brew tap wez/wezterm-linuxbrew
-    brew install wezterm
+    case $package_manager in
+      "apt")
+          curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/wezterm-fury.gpg
+          echo 'deb [signed-by=/etc/apt/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+          sudo apt update
+          install_package wezterm
+          ;;
+      "pacman")
+          yay -S wezterm-git
+          ;;
+      "dnf")
+          sudo dnf install -y https://github.com/wez/wezterm/releases/download/nightly/wezterm-nightly-fedora40.rpm
+          ;;
+      *)
+          echo "Unknown package manager"
+          exit 1
+          ;;
+  esac
 fi
 
 source packages/zsh_plugins.sh
